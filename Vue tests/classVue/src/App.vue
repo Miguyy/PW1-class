fb81c74aae84f832927d9d90e08c7148a66dde9c
Miguyy <miguelangelo123machado@gmail.com>
2025-10-06 12:22:42 +0100

    some tests

---
+++ Vue tests/classVue/src/App.vue
@@ -0,0 +1,42 @@
+<script setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <div>
+    {{ count }}
+  </div>
+  <button @click="double">double it</button>
+  <p>{{ message }}</p>
+  <p>{{ reverseMethod }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 5,
+      message: 'ESMAD',
+    }
+  },
+  methods: {
+    double() {
+      return (this.count = this.count * 2)
+    },
+    reverseMethod() {
+      return this.message.split('').reverse.join(' ')
+    },
+  },
+  beforeCreate() {
+    console.log(`beforeCreate ${this.count}`)
+  },
+  created() {
+    console.log(`created ${this.count}`)
+  },
+}
+</script>
+
+<style scoped></style>
